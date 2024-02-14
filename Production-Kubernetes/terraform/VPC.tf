faebad1794fafa21f67cc1b7ab6243bd68a35064
Fady <Fady@Fadyio.com>
2024-02-14 17:39:34 +0200

    Add argocd

---
+++ Production-Kubernetes/terraform/VPC.tf
@@ -0,0 +1,71 @@
+resource "aws_vpc" "EKS_VPC" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Prod VPC"
+  }
+}
+
+resource "aws_subnet" "pubsub1" {
+  vpc_id                  = aws_vpc.EKS_VPC.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "pubsub1-${var.environment}"
+  }
+}
+
+resource "aws_subnet" "pubsub2" {
+  vpc_id                  = aws_vpc.EKS_VPC.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+
+  tags = {
+    Name = "pubsub2-${var.environment}"
+  }
+}
+
+resource "aws_subnet" "privsub1" {
+  vpc_id            = aws_vpc.EKS_VPC.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "privsub1-${var.environment}"
+  }
+}
+
+resource "aws_subnet" "privsub2" {
+  vpc_id            = aws_vpc.EKS_VPC.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1d"
+
+  tags = {
+    Name = "privsub2-${var.environment}"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.EKS_VPC.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.EKS_VPC.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.pubsub1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.pubsub2.id
+  route_table_id = aws_route_table.rt.id
+}
